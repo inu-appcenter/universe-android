e7f66217213fc5c1bcbc48c10c8da7bd2f370de1
kimyekang <gongg10@naver.com>
2021-08-15 21:23:09 +0900

    signup

---
+++ app/src/main/java/org/inu/universe/feature/signup/SignupEmailCheckActivity.kt
@@ -0,0 +1,12 @@
+package org.inu.universe.feature.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.inu.universe.R
+
+class SignupEmailCheckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_email_check)
+    }
+}

115d562db6ae0115f552a93e999730ad9e0dec2d
kimyekang <gongg10@naver.com>
2021-08-17 23:03:56 +0900

    complete likelist UI

--- app/src/main/java/org/inu/universe/feature/like_list/LikeListFragment.kt
+++ app/src/main/java/org/inu/universe/feature/like_list/LikeListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import org.inu.universe.R
 
 class LikeListFragment : Fragment() {
@@ -14,6 +15,13 @@ class LikeListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_like_list, container, false)
+
+        val PersonInfoList = arrayListOf<PersonAdapter.PersonInfo>(
+            PersonAdapter.PersonInfo(R.drawable.easfa,"김예강", "21", "남")
+        )
+
+        val likePerson = view.findViewById<RecyclerView>(R.id.likeList_person)
+        likePerson.adapter = PersonAdapter(PersonInfoList)
 
         return view
     }
---
+++ app/src/main/java/org/inu/universe/feature/like_list/PersonAdapter.kt
@@ -0,0 +1,49 @@
+package org.inu.universe.feature.like_list
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.inu.universe.R
+
+class PersonAdapter(private val PersonList : List<PersonInfo>)
+    : RecyclerView.Adapter<PersonAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_likelist_person, parent, false)
+
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.name?.text = PersonList[position].name
+        holder.age?.text = PersonList[position].age
+        holder.gender?.text = PersonList[position].gender
+
+    }
+
+    override fun getItemCount(): Int = PersonList.size
+
+
+
+    inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+        var photo: TextView? = null
+        var name: TextView? = null
+        var age: TextView? = null
+        var gender: TextView? = null
+        init {
+            photo = view.findViewById(R.id.person_photo)
+            name = view.findViewById(R.id.person_name)
+            age = view.findViewById(R.id.person_age)
+            gender = view.findViewById(R.id.person_gender)
+        }
+    }
+
+    data class PersonInfo(val photo : Int,
+                          val name : String,
+                          val age : String,
+                          val gender : String)
+
+}

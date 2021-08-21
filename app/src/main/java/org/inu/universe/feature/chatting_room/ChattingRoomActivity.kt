769258de25bdb7cccbb6eaa9231a163648d95019
algosketch <sjy_9484@naver.com>
2021-08-21 19:17:47 +0900

    add drawer layout

--- app/src/main/java/org/inu/universe/feature/chatting_room/ChattingRoomActivity.kt
+++ app/src/main/java/org/inu/universe/feature/chatting_room/ChattingRoomActivity.kt
@@ -2,8 +2,12 @@ package org.inu.universe.feature.chatting_room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.Observer
 import org.inu.universe.R
 import org.inu.universe.databinding.ActivityChattingRoomBinding
 
@@ -16,5 +20,14 @@ class ChattingRoomActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_chatting_room)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        viewModel.shouldOpenDrawer.observe(this, Observer {
+            val drawer = findViewById<DrawerLayout>(R.id.chat_drawer)
+            if (it)
+                drawer.openDrawer(Gravity.RIGHT)
+            else
+                drawer.closeDrawer(Gravity.RIGHT)
+
+        })
     }
 }
--- app/src/main/java/org/inu/universe/feature/chatting_room/ChattingRoomViewModel.kt
+++ app/src/main/java/org/inu/universe/feature/chatting_room/ChattingRoomViewModel.kt
@@ -1,6 +1,12 @@
 package org.inu.universe.feature.chatting_room
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ChattingRoomViewModel : ViewModel() {
+    val shouldOpenDrawer = MutableLiveData(false)
+
+    fun onDrawerMenuClick() {
+        shouldOpenDrawer.postValue(true)
+    }
 }
